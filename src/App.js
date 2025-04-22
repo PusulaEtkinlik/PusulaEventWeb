dae3f83d456a6c7865a2343bb9469c5be691cdbb
Furkan AYDOĞAN <pusulaegeetkinlik@gmail.com>
2025-04-22 15:39:10 +0300

    🔒 Add private route protection for dashboard and other secure pages

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ResetPasswordWithApproval from "./components/ResetPasswordWithApproval";
 import PhoneAuth from "./components/PhoneAuth";
 import EditEvent from "./components/EditEvent";
 import AnalyticsPage from "./components/AnalyticsPage";
+import PrivateRoute from "./components/PrivateRoute";
 function App() {
   return (
     <Router>
@@ -22,17 +23,105 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/register-sms" element={<AdminRegisterWithSmsTo />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/create-event" element={<CreateEvent />} />
-          <Route path="/invite" element={<InviteGuest />} />
-          <Route path="/check-in" element={<CheckIn />} />
-          <Route path="/import" element={<ImportPage />} />
-          <Route path="/events" element={<EventsPage />} />
-          <Route path="/events/:id/guests" element={<GuestListPage />} />
-          <Route path="/reset-password" element={<ResetPasswordWithApproval />} />
-          <Route path="/reset-password-auth" element={<PhoneAuth />} />
-          <Route path="/edit-event/:id" element={<EditEvent />} />
-          <Route path="/analytics" element={<AnalyticsPage />} />
+
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/create-event"
+            element={
+              <PrivateRoute>
+                <CreateEvent />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/invite"
+            element={
+              <PrivateRoute>
+                <InviteGuest />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/check-in"
+            element={
+              <PrivateRoute>
+                <CheckIn />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/import"
+            element={
+              <PrivateRoute>
+                <ImportPage />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/events"
+            element={
+              <PrivateRoute>
+                <EventsPage />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/events/:id/guests"
+            element={
+              <PrivateRoute>
+                <GuestListPage />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/reset-password"
+            element={
+              <PrivateRoute>
+                <ResetPasswordWithApproval />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/reset-password-auth"
+            element={
+              <PrivateRoute>
+                <PhoneAuth />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/edit-event/:id"
+            element={
+              <PrivateRoute>
+                <EditEvent />
+              </PrivateRoute>
+            }
+          />
+
+          <Route
+            path="/analytics"
+            element={
+              <PrivateRoute>
+                <AnalyticsPage />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </div>
     </Router>
