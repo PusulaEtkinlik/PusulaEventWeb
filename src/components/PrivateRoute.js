16d3a10df63fed01c031871043fa9e663395375d
Furkan AYDOĞAN <pusulaegeetkinlik@gmail.com>
2025-04-22 16:16:18 +0300

    🔒 Add useeffect for privateroute

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,20 @@
+// src/components/PrivateRoute.js
 import { Navigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect, useState } from "react";
 import { auth } from "../firebase";
 
 export default function PrivateRoute({ children }) {
   const [user, loading] = useAuthState(auth);
+  const [ready, setReady] = useState(false);
 
-  if (loading) return <div>Yükleniyor...</div>;
+  useEffect(() => {
+    if (!loading) {
+      setReady(true);
+    }
+  }, [loading]);
+
+  if (!ready) return <div>Yükleniyor...</div>;
   if (!user) return <Navigate to="/" replace />;
 
   return children;
